67bc8ca1d609c7dffd4c5604d380f251153bf999
mahendrapanwar2003 <mahendrapanwar269@gmail.com>
2025-08-26 20:01:15 +0530

    first

--- src/utils/API/URL.js
+++ src/utils/API/URL.js
@@ -1,4 +1,4 @@
 export const mediaUrl = () => `https://golfserver.appsxperts.live`;
 
 // export const APIURL = () => `http://localhost:8001`;
-export const APIURL = () => `http://3.89.228.105:8000`;
+export const APIURL = () => `https://server.profinderr.co.uk`;
